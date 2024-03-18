c1c664066b896bf025d5d805c683b6fac3ff2ad2
邓锐哲 <12722854+kalaxiaoke777@user.noreply.gitee.com>
2024-03-18 11:27:31 +0800

    地图初始化

---
+++ src/components/search/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <view>
+    <input
+      type="text"
+      class="Finput"
+      @input="inputChange"
+      placeholder="搜索钓场"
+    />
+  </view>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+const inputValue = ref("");
+onMounted(() => {});
+const inputChange = (e: any) => {
+  inputValue.value = e.target.value;
+};
+</script>
+
+<style lang="scss" scoped>
+.Finput {
+  position: fixed;
+  width: 75vw;
+  top: 10px;
+  left: 50%;
+  transform: translateX(-50%);
+  z-index: 9999;
+  border: 2px solid #ccc;
+  border-radius: 10px;
+  padding: 5px;
+}
+</style>
--- src/pages/fishingMap/fishingMap.vue
+++ src/pages/fishingMap/fishingMap.vue
@@ -1,13 +1,29 @@
 <template>
-    <view>
-        fishingMap
+  <view>
+    <view class="page-body">
+      <view class="page-section page-section-gap">
+        <map
+          @tap="handleMapTap"
+          style="width: 100%; height: 100vh"
+          :latitude="coordinates[1]"
+          :longitude="coordinates[0]"
+        >
+        </map>
+      </view>
     </view>
+  </view>
+  <search />
 </template>
 
 <script lang="ts" setup>
-
+import search from "@/components/search/index.vue";
+import { ref } from "vue";
+const coordinates = ref([104, 30]);
+const handleMapTap = (e: any) => {
+  console.log(e);
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
+netstat -an | find "10025"
