eff9bb8f0ba0768058e71d021df52107c4f9cf97
邓锐哲 <12722854+kalaxiaoke777@user.noreply.gitee.com>
2024-03-21 11:58:02 +0800

    小程序的ip定位

--- src/components/tools/index.vue
+++ src/components/tools/index.vue
@@ -1,19 +1,48 @@
 <template>
     <view class="tools">
-        <image src="@/static/fishing/路况.png" class="toolImage" @click="changeTraffic"></image>
+        <image src="@/static/fishing/路况.png" class="toolImage" @click="changeIsShow" :data-message="{message:'实时路况',type:'isTraffic',func:'toggleTraffic'}"></image>
+        <image src="@/static/fishing/卫星地图.png" :style="{width:'32px',height:'32px'}" @click="changeIsShow" :data-message="{message:'卫星地图',type:'isEnableSatellite',func:'toggleEnableSatellite'}"></image>
     </view>
 </template>
 
 <script lang="ts" setup>
-import type { PropType } from 'vue';
-
-// 声明接收的 traffic 属性
+import { onLoad } from '@dcloudio/uni-app';
+import { ref, type PropType } from 'vue';
+const exeRet = ref("")
+const title = ref("")
+interface ToolsProps {
+  func: any
+}
+// 定义组件接收的props
 const props = defineProps({
-    toggleTraffic: Function as PropType<() => void>
+  func: {
+    type: Object as () => ToolsProps['func'],
+    required: true
+  }
 });
-
-const changeTraffic = ()=>{
-    props.toggleTraffic && props.toggleTraffic();
+const changeIsShow = (event: any)=>{  
+    const message = event.currentTarget.dataset.message.message
+    const type = event.currentTarget.dataset.message.type
+    const func = event.currentTarget.dataset.message.func
+    const funct = props.func[func]
+    console.log(funct,func);
+    
+    funct && funct();
+    uni.showToast({
+          title: props.func.isShow[type] ? "打开"+message : "关闭"+message,
+          success: (res) => {
+            exeRet.value = "success:" + JSON.stringify(res)
+          },
+          fail: (res) => {
+            exeRet.value = "fail:" + JSON.stringify(res)
+          },
+        })
+        setTimeout(() => {
+            uni.hideToast()
+        }, 1000);
+}
+const changeEnableSatellite = ()=>{
+    props.func.toggleEnableSatellite && props.func.toggleEnableSatellite();
 }
 </script>
 
--- src/pages/fishingMap/fishingMap.vue
+++ src/pages/fishingMap/fishingMap.vue
@@ -4,12 +4,15 @@
       <view class="page-section page-section-gap">
         <map
           enable-3D = "true"
-          :enable-overlooking = "true"
-          :enable-traffic = "traffic"
+          :enable-overlooking = "isShow.isOverlooking"
+          :enable-traffic = "isShow.isTraffic"
+          :enable-satellite	="isShow.isEnableSatellite	"
           @tap="handleMapTap"
           style="width: 100%; height: 100vh"
           :latitude="coordinates[1]"
           :longitude="coordinates[0]"
+          :circles="data.circles"
+          :scale="data.scale"
         >
         </map>
       </view>
@@ -17,18 +20,78 @@
   </view>
   <searchPoint />
   <addPoint />
-  <Tools :toggleTraffic="toggleTraffic"/>
+  <Tools :func="{toggleTraffic,toggleEnableSatellite,isShow}"/>
 </template>
 
 <script lang="ts" setup>
 import addPoint from "@/components/addPoint/index.vue";
 import searchPoint from "@/components/search/index.vue";
 import Tools from "@/components/tools/index.vue";
+import { onLoad, onShow } from "@dcloudio/uni-app";
+import { onMounted } from "vue";
 import { ref } from "vue";
-const coordinates = ref([104.0668, 30.5728]);
-const traffic = ref(false);
+const coordinates = ref([104, 35]);
+const isShow = ref({
+  isOverlooking:false,
+  isTraffic:false,
+  isEnableSatellite:false,
+});
+const data = ref({
+  circles:[{
+    latitude: 35,
+    longitude: 104,
+    fillColor: "#D9E6EF", //填充颜色
+    color: "#A7B6CB", //描边的颜色 .
+    radius: 1, //半径
+    strokeWidth: 2 //描边的宽度
+  }],
+  scale:1
+})
+onMounted(()=>{
+  console.log("onLoad");
+  uni.authorize({
+      scope: 'scope.userLocation',
+      success() {
+        // 用户已授权，可以获取地理位置信息
+        uni.getLocation({
+          type: 'wgs84', // 返回可以用于uni.openLocation的经纬度
+          success: (res) => {
+            coordinates.value[1] = res.latitude
+			      coordinates.value[0] = res.longitude
+            data.value.scale = 15
+            data.value.circles = [{ //在地图上显示圆
+              latitude: res.latitude,
+              longitude: res.longitude,
+              fillColor: "#D9E6EF", //填充颜色
+              color: "#A7B6CB", //描边的颜色
+              radius: 15, //半径
+              strokeWidth: 2 //描边的宽度
+            }]
+          },
+          fail: () => {
+            // 获取地理位置失败的处理逻辑
+            uni.showToast({
+              title: '无法获取地理位置',
+              icon: 'none'
+            });
+          }
+        });
+      },
+      fail() {
+        // 用户拒绝授权，你可以引导用户到设置页面开启权限
+        uni.showToast({
+          title: '请在系统设置中打开位置权限',
+          icon: 'none'
+        });
+      }
+    });
+})
+
 const toggleTraffic = () => {
-  traffic.value = !traffic.value;
+  isShow.value.isTraffic = !isShow.value.isTraffic;
+}
+const toggleEnableSatellite = () => {
+  isShow.value.isEnableSatellite = !isShow.value.isEnableSatellite;
 }
 const handleMapTap = (e: any) => {
   console.log(e);
